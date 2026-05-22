f92146ed2cea41fd64caed54cb644309e10f2ba2
sakisan <sakisan@st.rim.or.jp>
2026-05-22 15:16:20 +0900

    enum を追加

--- list_resolution.cpp
+++ list_resolution.cpp
@@ -11,14 +11,14 @@ int main() {
     dm.dmSize = sizeof(dm);
 
     std::cout << "Enumerating available display resolutions..." << std::endl;
-    std::cout << "Width x Height \t Refresh Rate \t Color Depth" << std::endl;
+    std::cout << "Enum\t Width x Height \t Refresh Rate \t Color Depth" << std::endl;
     std::cout << "---------------------------------------------------" << std::endl;
 
     // Use EnumDisplaySettings to iterate through all supported modes.
     // ENUM_CURRENT_SETTINGS or ENUM_REGISTRY_SETTINGS can be used for specific modes,
     // but passing an index (starting at 0) retrieves all supported modes.
     for (int i = 0; EnumDisplaySettings(NULL, i, &dm) != 0; i++) {
-        std::cout << dm.dmPelsWidth << " x " << dm.dmPelsHeight
+        std::cout << i << "\t " << dm.dmPelsWidth << " x " << dm.dmPelsHeight
                   << "\t " << dm.dmDisplayFrequency << " Hz"
                   << "\t " << dm.dmBitsPerPel << " bit" << std::endl;
     }
